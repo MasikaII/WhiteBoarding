13c4a859a2fc43f1d059ce51264acb37e306c6e5
erickadikah <erickadikah2030@gmail.com>
2023-01-09 00:44:38 +0300

    new changes appended

--- flusk_blog.py
+++ flusk_blog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '33a849461854d67eb2f5e573391ce702'
 posts = [
     {
         'author': 'Erick Adikah',
@@ -22,9 +24,24 @@ posts = [
 @app.route("/home/")
 def home():
     return render_template('home.html', posts=posts)
+
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Acount created for {form.username.data}!')
+    return render_template('register.html', title='Registar', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('Login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
