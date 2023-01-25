f2d8a31ee7f4beadb3501ef97d09944729acf9f1
MasikaII <48204233+MasikaII@users.noreply.github.com>
2023-01-25 19:00:44 +0300

    frontend

--- Project/ML_Classification/Codex/client/script.js
+++ Project/ML_Classification/Codex/client/script.js
@@ -1,23 +1,68 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+// importing icons from assets
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+// targeting our html elements
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+// function that loads our messages
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '...') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+// function that implement typing functionality
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index); // gets the character under a specific index in the txt AI returns
+      index++;
+    }
+    else { // if we've reached the end of the text
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+// function that generates a unique id for every message to be able to map over them
+function generateUniqueId() {
+  // creating unique id using current date and time and a random number
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+// function that implements chat stripe
+function chatStripe(isAI, value, uniqueId) {
+  return (
+    `
+    <div class="wrapper ${isAI && 'AI'}">
+      <div class="chat">
+        <div className="profile">
+          <img
+            src="${isAI ? bot : user}"
+            alt="${isAI ? 'bot' : 'user'}"
+          />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+    `
+  )
+}
 
-setupCounter(document.querySelector('#counter'))
+// function that acts as trigger to obtain the AI generated response
